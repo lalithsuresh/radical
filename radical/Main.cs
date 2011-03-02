8a415f2c0b35360279be8b0f496033a21b14d6ff
Lalith Suresh <suresh.lalith@gmail.com>
2011-03-02 19:54:12 +0000

    Empty project time

--- radical/Main.cs
+++ radical/Main.cs
@@ -6,11 +6,7 @@ namespace radical
 	class MainClass
 	{
 		public static void Main (string[] args)
-		{
-			Application.Init ();
-			MainWindow win = new MainWindow ();
-			win.Show ();
-			Application.Run ();
+		{			
 		}
 	}
 }
