46f31b947b37268584e04ff74875658f1d298b70
Lalith Suresh <suresh.lalith@gmail.com>
2011-03-19 21:15:05 +0000

    Added service register callback mechanism

--- comm/SendReceiveMiddleLayer.cs
+++ comm/SendReceiveMiddleLayer.cs
@@ -1,8 +1,13 @@
 using System;
+using System.Collections.Generic;
 namespace comm
 {
+	public delegate void ReceiveCallbackType(object sender, EventArgs e);
+	
 	public class SendReceiveMiddleLayer : PadicalObject
 	{
+		private Dictionary<string, ReceiveCallbackType> m_registerMap = new Dictionary<string, ReceiveCallbackType> ();
+		
 		public SendReceiveMiddleLayer ()
 		{
 		}
@@ -10,6 +15,10 @@ namespace comm
 		public void Send () 
 		{
 		}
+		
+		public void RegisterReceiveCallback (String service, ReceiveCallbackType cb)
+		{
+			m_registerMap.Add (service, cb);
+		}
 	}
 }
-
