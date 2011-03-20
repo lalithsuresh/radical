5f645a914292f14daff7d17e8d06235275d2b9e6
Marcus Ljungblad <marcus@ljungblad.nu>
2011-03-20 00:33:13 +0000

    basic skeleton for sequence numbering service

--- server/SequenceNumberServiceServer.cs
+++ server/SequenceNumberServiceServer.cs
@@ -1,4 +1,5 @@
 using System;
+using comm;
 namespace server
 {
 	public class SequenceNumberServiceServer : IServiceServer
@@ -11,23 +12,37 @@ namespace server
 			m_sequenceNumber = 0;
 		}
 		
-		public long GetSequenceNumber ()
-		{
-			m_sequenceNumber++;
-			return m_sequenceNumber;
-		}
-		
 		public void SetCallback (Server server) 
 		{
 			if (server != null) 
 				m_server = server;
 		}
 		
-		public void Receive (Message message)
+		
+		/**
+		 * Get a unique sequence number
+		 */
+		public long GetSequenceNumber ()
 		{
+			m_sequenceNumber++;
+			return m_sequenceNumber;
 		}
 		
 		
+		public void Receive (Message message)
+		{
+			string source = message.GetSource ();
+			string request = message.PopString ();
+			SendReply (source, GetSequenceNumber ());
+		}
+		
+		private void SendReply (string destination, long no) 
+		{
+			Message reply = new Message ();
+			reply.SetDestination (destination);
+			reply.PushString (no.ToString ());
+			m_server.Send (reply);
+		}
 	}
 }
 
