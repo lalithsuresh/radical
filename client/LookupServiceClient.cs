a9ee0b1b659f839229e6965d7f0414f6252a9239
Lalith Suresh <suresh.lalith@gmail.com>
2011-03-20 00:22:36 +0000

    Adds blocking request enabled LookupService and SequenecNumberService at client side

--- client/LookupServiceClient.cs
+++ client/LookupServiceClient.cs
@@ -1,11 +1,31 @@
 using System;
+using System.Threading;
+using System.Runtime.CompilerServices;
+
 namespace client
 {
 	public class LookupServiceClient : comm.PadicalObject
 	{
+		ManualResetEvent oSignalEvent = new ManualResetEvent (false);
+		
 		public LookupServiceClient ()
 		{
 		}
+		
+		[MethodImpl(MethodImplOptions.Synchronized)]
+		public void Lookup ()
+		{
+			//This thread will block here until the reset event is sent.
+			Console.WriteLine ("Sent Lookup request");
+			oSignalEvent.WaitOne();
+			Console.WriteLine ("Releasing block");
+			oSignalEvent.Reset ();
+		}
+		
+		public void Receive ()
+		{
+			Console.WriteLine ("Response received");
+			oSignalEvent.Set ();
+		}
 	}
 }
-
